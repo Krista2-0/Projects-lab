d42f1560838cd53f83af0902b7c0104442d8150b
Krista 2.0 <lysofsnow@gmail.com>
2026-01-18 14:34:15 +0000

    Create maths.js
    Basic Math calculations

---
+++ java script/maths.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+    console.log(a + b);
+}
+
+function subtract(a, b) {
+    console.log(a - b);
+}
+
+function multiply(a, b) {
+    console.log(a * b);
+}
+
+add(2, 4);
+subtract(4, 5);
+multiply(2, 4);
